db9ea8c4abe7f7e7383c7ed7e5881fb2412d6fd1
gukj <gustav.kjellin@me.com>
2017-05-03 14:18:02 +0200

    Add more images to model

--- app/models/book.js
+++ app/models/book.js
@@ -3,7 +3,9 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   description: DS.attr('string'),
   editionSize: DS.attr('string'),
-  image: DS.attr('string'),
+  imageFront400: DS.attr('string'),
+  imageFront800: DS.attr('string'),
+  imageSide: DS.attr('string'),
   itemNo: DS.attr('string'),
   name: DS.attr('string'),
   release: DS.attr('string')
